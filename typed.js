c49f363064b88ae53431acd8b4a9cc7117d872e0
Midenlex Joseph <midenlex.joseph@gmail.com>
2016-06-08 21:11:40 -0400

    commit

--- typed.js
+++ typed.js
@@ -271,7 +271,7 @@
                 // the stopNum actually represents the amount of chars to
                 // keep in the current string. In my case it's 14.
              if (self.arrayPos == 1){
-              self.stopNum = 3;
+              self.stopNum = 14;
                 }
                 //every other time, delete the whole typed string
                  else{
