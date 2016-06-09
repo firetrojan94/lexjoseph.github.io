ecc854ce4cd103a74c23fc8e34505b3090daceb7
Midenlex Joseph <midenlex.joseph@gmail.com>
2016-06-08 20:59:24 -0400

    commit

--- typed.js
+++ typed.js
@@ -270,13 +270,15 @@
                 // on the first string, only delete one word
                 // the stopNum actually represents the amount of chars to
                 // keep in the current string. In my case it's 14.
-                // if (self.arrayPos == 1){
-                //  self.stopNum = 14;
-                // }
+                // if 
+                    do{
+                      self.stopNum = 11;
+                      }
+                while (self.arrayPos == 1)
                 //every other time, delete the whole typed string
                 // else{
                 //  self.stopNum = 0;
-                // }
+                 }
 
                 if (self.contentType === 'html') {
                     // skip over html tags while backspacing
